c09efa618cf003340830d3155c4f60871974ae3b
Chetan Yewale <yewalechetan@gmail.com>
2017-12-11 18:12:29 +0000

    Shows how to add image in template

---
+++ src/main/java/com/coreoz/ppt/TestPPTTemplates.java
@@ -0,0 +1,62 @@
+package com.coreoz.ppt;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class TestPPTTemplates {
+
+	public static void main(String[] args) {
+
+		final String imagePath = "C:\\Users\\Chetan\\Pictures\\notfound.jpg";
+		byte[] jpgByteArray = null;
+
+		try {
+			BufferedImage image = ImageIO.read(new File(imagePath));
+			ByteArrayOutputStream b = new ByteArrayOutputStream();
+			ImageIO.write(image, "jpg", b);
+
+			jpgByteArray = b.toByteArray();
+			
+			
+			// byte[] imageData - needs to be found.
+
+			try (FileOutputStream out = new FileOutputStream("E:\\generated.pptx")) {
+				try {
+					new PptMapper().text("title", "Hello").text("secondline", "SecondLine")
+									.text("firstline", "FirstLine")
+									.image("image1", jpgByteArray, PptImageReplacementMode.RESIZE_ONLY )
+							        .processTemplate(new FileInputStream("E:\\Template.pptx")).write(out);
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} catch (FileNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+
+		} catch (IOException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+
+
+
+	}
+
+}
