7fbd0fbfb5792d2790e79d46e5a6f90553c9dc78
Chetan Yewale <yewalechetan@gmail.com>
2017-12-12 17:40:33 +0000

    Added an example in TestPPTTemplates which adds multiple text runs.

--- src/main/java/com/coreoz/ppt/TestPPTTemplates.java
+++ src/main/java/com/coreoz/ppt/TestPPTTemplates.java
@@ -9,12 +9,103 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
 
+import org.apache.poi.xslf.usermodel.XMLSlideShow;
+import org.apache.poi.xslf.usermodel.XSLFShape;
+import org.apache.poi.xslf.usermodel.XSLFSlide;
+import org.apache.poi.xslf.usermodel.XSLFTextShape;
+
 public class TestPPTTemplates {
 
+	
 	public static void main(String[] args) {
+		
+		testMultipleParaGen();
+	}
+	
+	
+	public static void testMultipleParaGen( ) {
+		
+		List<String> myTextList = new ArrayList<String>( Arrays.asList(  "apples", "nectarines", "guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ,
+				"guavas" ) )  ;
+		
+		try {
+			final XMLSlideShow xmlSlideShow = new XMLSlideShow(new FileInputStream("E:\\test.pptx"));
+			XSLFSlide slide =  xmlSlideShow.getSlides().get(0) ;
+			
+			List<XSLFShape> shapeList = slide.getShapes()
+					                          .stream()
+					                          .filter( (shape) -> shape instanceof XSLFTextShape  )
+					                          .collect(Collectors.toCollection(ArrayList::new)) ;
+			
+			
+			java.awt.Dimension pgsize = xmlSlideShow.getPageSize();
+		    int pgx = pgsize.width; //slide width in points
+		    int pgy = pgsize.height; //slide height in points
+			
+		    
+		    //you can detect when the anchor of the containing text box is overflowing.
+		    //in that case, create new slide and create the shape on new slide.
+			if(shapeList.get(0) == null) {
+				return;
+			}else {
+			
+				CreateMultipleTextParagraphs.createMultipleTextParagraphs(myTextList,   (XSLFTextShape) shapeList.get(0) );
+				
+			}
+			
+			xmlSlideShow.write(new FileOutputStream("E:\\test.pptx"));
+			xmlSlideShow.close();
+			
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+
+	}
+	
+	
+	
+	public static void testPPTGenFromTemplate(String[] args) {
 
 		final String imagePath = "C:\\Users\\Chetan\\Pictures\\notfound.jpg";
 		byte[] jpgByteArray = null;
